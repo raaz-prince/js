e2253ccc5c594d20627e04a499efa8764032d905
raaz-prince <krprince006@gmail.com>
2023-10-01 22:51:52 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";  //means we are using latest modules 
+
+// alert( 3 +3 )  //we are using nodejs , not browser
+
+let name = "prince"  //string data type
+let age = 19  //number
+let isLoggedIn = false   //boolean
+
+// number => 2 to power 53 
+//bigint
+
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>  
+// symbol => uniqueness
+
+// object 
+
+console.log(typeof name)  // used to get type of variables
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
