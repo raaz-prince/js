5c316f72a232f169955a259f5165f62e681af39e
raaz-prince <krprince006@gmail.com>
2023-10-01 22:48:35 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const account_Id = 21518 
+let accountEmail = "krprince006@gmail.com"
+var account_Password = "12345"  //not generally used
+accountCity = "Jaipur"
+
+//account_Id = 2  //not allowed as it is constant
+
+console.log(account_Id)
+console.table([accountEmail,account_Id,account_Password,accountCity])
