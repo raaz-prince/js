3a4d1d02a154332d413d95caebdee35e8341db65
raaz-prince <krprince006@gmail.com>
2023-10-01 23:26:33 +0530

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,21 +1,21 @@
 let score = "33";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 let sc = "33abc";
-console.log(typeof sc);
+//console.log(typeof sc);
 
 let value = Number(sc);
 
-console.log(typeof value);
+//console.log(typeof value);
 
-console.log(value); // it's output is NaN which means not a number
+//console.log(value); // it's output is NaN which means not a number
 
 /* 
 "33" => 33
@@ -25,5 +25,18 @@ true =>1 ; false => 0;
 
 let is = 1
 let booleanIs = Boolean(is);
-console.log(booleanIs);
-console.log(typeof booleanIs);
+//console.log(booleanIs);
+//console.log(typeof booleanIs);
+
+//********************* OPERATIONS************** */
+
+console.log(2+2)
+console.log(1+"2")
+console.log(1+"2")
+console.log("1"+2+2)  //122
+
+console.log(1+2+"2")   //32
+//if string is at first then all are treated as string and if string is present at last then the conversion is done before string
+
+console.log((3+4) *3 %7)
+
