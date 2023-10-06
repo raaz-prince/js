9c4a64551b793cacc67d0919c06cffa041607547
raaz-prince <krprince006@gmail.com>
2023-10-07 01:34:59 +0530

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -12,7 +12,7 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullName.userFullName.firstName);
+//console.log(regularUser.fullName.userFullName.firstName);
 
 const obj1 = {1 :"a", 2 : "b"} 
 const obj2 = {3 :"a", 4 : "b"} 
@@ -21,7 +21,39 @@ const obj2 = {3 :"a", 4 : "b"}
 //const obj3= Object.assign({},obj1,obj2) //gurantee combination by using curly braces "{}" used as target
 
 const obj3 = {...obj1 , ...obj2}
-console.log(obj3);
+//console.log(obj3);
 
-console.log(Object.keys(regularUser));
-console.log(Object.entries(regularUser)); //get key , value pair
+//console.log(Object.keys(regularUser));
+//console.log(Object.entries(regularUser)); //get key , value pair
+
+
+
+const  course = {
+    coursename : "js in sanskrit",
+    price : "999",
+    courseInstructor : "prince"
+}
+
+//course.courseInstructor may be written as below syntax when we have to use it more no. of times
+const {courseInstructor : instructor} = course ;  // value destructuring
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+// API : Application Program Interface 
+
+
+/*  These data are in json format of object type
+{
+     "name" : "prince",
+     "coursename" : "js in hindi",
+     "prince" : "free"
+ } */
+   
+
+ // array type api handling
+[
+    {},
+    {},
+    {}
+]
