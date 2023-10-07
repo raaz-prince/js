7ac85c99ec670a0a1bd152c8d7a8e17682b79a4a
raaz-prince <krprince006@gmail.com>
2023-10-07 17:09:04 +0530

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+let a=5;
+while (a<=10) {
+    console.log(`${a}  prince`);
+    a++
+}
+
+let myArr = ["flash","superman","batman","wonder woman"]
+let arr =0;
+while(arr < myArr.length) {
+    console.log(`${arr+1} ${myArr[arr]}`);
+    arr++;
+}
+
+
+
+// do-while   first do and then start checking condition
+
+let b=1
+do {
+    console.log(`${b}  prince`)
+    b++
+} while (b<=5) ;
