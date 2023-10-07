d48d12ca25faac05121b618eac0453f1599f752d
raaz-prince <krprince006@gmail.com>
2023-10-07 12:27:12 +0530

    different fun

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -12,6 +12,42 @@ if(true) {
     const b = 60
     var c = 16
 }
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() 
+{
+  const userName = "prince" 
+
+  function two() {
+    const website = "youTube"
+    console.log(userName)
+  }
+
+  //console.log(website);   //can't be accessed bcz it is not its scope as  child  can access parent's variable but parentt can't access child's variable
+
+  two()
+}
+
+
+one()
+
+console.log(add(9));
+function add(num) {
+  return num +1;
+}
+
+
+
+  //this is called expression sometime
+
+  // console.log(sum(5)) can't access before initialization
+    const sum = function(num) {
+  return num +1;
+}
+
+console.log(sum(5))
+
+
+
