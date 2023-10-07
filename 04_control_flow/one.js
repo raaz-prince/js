764dd299ebc8439e8798c90adf59d13ae90ea2bd
raaz-prince <krprince006@gmail.com>
2023-10-07 16:05:24 +0530

    if

---
+++ 04_control_flow/one.js
@@ -0,0 +1,11 @@
+//if  if(condn == true) then execute it statements otherwise else statements
+
+let temperature = 60
+
+if(temperature < 50) {
+    console.log(`temperature is less than 50`);
+}
+
+else 
+console.log(`temperature greater than 50`);
+
