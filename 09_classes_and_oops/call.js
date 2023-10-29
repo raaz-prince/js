06c369a69b4e56734eb30f39dfb74104d52c1d3c
raaz-prince <krprince006@gmail.com>
2023-10-30 00:38:59 +0530

    call in js

--- 09_classes_and_oops/call.js
+++ 09_classes_and_oops/call.js
@@ -1,4 +1,8 @@
 // call is used when we want to set a variable of a function by the help of other function then we have to use call with function name
+
+//  call() => function borrowing
+
+
 function setUsername(username) {
     this.username = username
     }
@@ -11,5 +15,15 @@ function setUsername(username) {
     }
     
     const us = new createUser("prince","pr@prince.com","razz-prince")
-    
+
     console.log(us)
+
+
+    //Another function
+    
+    function user(username) {
+        setUsername.call(this,username)
+    }
+    
+    const us1 = new user("prince kumar")
+    console.log(us1.username)
