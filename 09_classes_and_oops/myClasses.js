096392e9fa331c493e9e6dbcf68b9afcc3c6b5b8
raaz-prince <krprince006@gmail.com>
2023-10-29 23:46:12 +0530

    classes implementation by functioni

--- 09_classes_and_oops/myClasses.js
+++ 09_classes_and_oops/myClasses.js
@@ -12,3 +12,22 @@ class user {
 
 const pk = new user("prince","123","pk@gmail.com")
 console.log(pk.encryptPassword())
+
+//behind the scene
+
+function User(username, email, password) {
+    this.username= username
+    this.email = email
+    this.password = password
+}
+
+User.prototype.encryptPassword = function() {
+    return `${this.password}abc`
+}
+
+User.prototype.changeUserName = function() {
+    return this.username.toUpperCase()
+}
+
+const dk = new User("pri","krp@gmail.com","jlklkjf")
+console.log(dk.changeUserName())
